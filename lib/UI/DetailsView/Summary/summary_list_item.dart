2fe31a6f3c42fc77a08fc4579bd20e2756cfb359
czbalint <62812514+czbalint@users.noreply.github.com>
2022-08-11 10:48:04 +0200

    avatar add to list item

--- lib/UI/DetailsView/Summary/summary_list_item.dart
+++ lib/UI/DetailsView/Summary/summary_list_item.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutterhf/UI/DetailsView/Member/avatar.dart';
 import 'package:flutterhf/UI/DetailsView/Member/member.dart';
 
 class MemberListItem extends StatefulWidget {
@@ -16,45 +17,70 @@ class MemberListItem extends StatefulWidget {
 class _MemberListItemState extends State<MemberListItem> {
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width * 0.7;
+
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(30),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        width: 50,
-        height: 70,
-        child: Stack(
-          children: [
-            BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-              child: Container(),
-            ),
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 50, right: 20),
+            width: 300,
+            height: 80,
+           
+            child: Stack(
+              children: [
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(),
+                ),
 
-            Container(
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.lightGreenAccent, width: 3),
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Theme.of(context).primaryColor.withOpacity(0.5),
+                        Theme.of(context).primaryColor.withOpacity(0.1),
+                      ]
+                    )
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(left: 40),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Nev: ${widget.currentMember.name} Tartozas: ${widget.debitSum}")
+                  ),
+                ),
+
+
+            ],
+          ),
+        ),
+          Positioned(
+            left: 25,
+            top: 11,
+            child: Container(
+              width: 60,
+              height: 60,
+              padding: EdgeInsets.only(top: 3, left: 4),
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.lightGreenAccent, width: 5),
                 borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Theme.of(context).primaryColor.withOpacity(0.5),
-                    Theme.of(context).primaryColor.withOpacity(0.1),
-                  ]
-                )
+                color: Colors.lightGreenAccent,
+
+              ),
+              child: MemberAvatar(
+                svgCode: widget.currentMember.avatar!,
+                size: const Size(53, 53),
               ),
             ),
-            
-            Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Nev: ${widget.currentMember.name} Tartozas: ${widget.debitSum}")
-              ),
-            )
-          ],
-        ),
+          )
+        ]
       ),
     );
   }
