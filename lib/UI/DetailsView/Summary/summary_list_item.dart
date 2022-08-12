bd93a2ca4dcfb2d61009937ec0e72fab25f3cde7
czbalint <62812514+czbalint@users.noreply.github.com>
2022-08-13 00:09:47 +0200

    list item styling

--- lib/UI/DetailsView/Summary/summary_list_item.dart
+++ lib/UI/DetailsView/Summary/summary_list_item.dart
@@ -15,10 +15,18 @@ class MemberListItem extends StatefulWidget {
 }
 
 class _MemberListItemState extends State<MemberListItem> {
+  List<Color> greenGradient = [
+    Colors.lightGreen.withOpacity(0.5),
+    Colors.lightGreen.withOpacity(0.1),
+  ];
+
+  List<Color> redGradient = [
+    Colors.redAccent.withOpacity(0.5),
+    Colors.redAccent.withOpacity(0.1),
+  ];
+
   @override
   Widget build(BuildContext context) {
-
-
     return ClipRRect(
       borderRadius: BorderRadius.circular(30),
       child: Stack(
@@ -27,7 +35,7 @@ class _MemberListItemState extends State<MemberListItem> {
             margin: const EdgeInsets.only(left: 50, right: 20),
             width: 300,
             height: 80,
-           
+
             child: Stack(
               children: [
                 BackdropFilter(
@@ -42,23 +50,74 @@ class _MemberListItemState extends State<MemberListItem> {
                     gradient: LinearGradient(
                       begin: Alignment.topLeft,
                       end: Alignment.bottomRight,
-                      colors: [
-                        Theme.of(context).primaryColor.withOpacity(0.5),
-                        Theme.of(context).primaryColor.withOpacity(0.1),
-                      ]
+                      colors: widget.debitSum < 0.0 ? redGradient : greenGradient
                     )
                   ),
                 ),
 
                 Padding(
                   padding: const EdgeInsets.only(left: 40),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text("Nev: ${widget.currentMember.name} Tartozas: ${widget.debitSum}")
-                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 5),
+                                child: Text(
+                                  widget.currentMember.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17
+                                  ),
+                                ),
+                              ),
+                              const Text(
+                                "Név",
+                                style: TextStyle(
+                                  fontStyle: FontStyle.italic
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 30),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 5),
+                                child: Text(
+                                  widget.debitSum.toString(),
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17
+                                  ),
+                                ),
+                              ),
+                              const Text(
+                                "Összeg",
+                                style: TextStyle(
+                                    fontStyle: FontStyle.italic
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  )
                 ),
-
-
             ],
           ),
         ),
@@ -68,7 +127,7 @@ class _MemberListItemState extends State<MemberListItem> {
             child: Container(
               width: 60,
               height: 60,
-              padding: EdgeInsets.only(top: 3, left: 4),
+              padding: const EdgeInsets.only(top: 3, left: 4),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
                 color: Colors.lightGreenAccent,
